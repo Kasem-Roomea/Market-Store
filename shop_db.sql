271006cc4e57781e1777d3f5e03b93f8e70a7e0b
Kasem-Roomea <kasem711roomea@gmail.com>
2023-02-19 13:53:27 +0200

    dataBase Sql Commit

--- shop_db.sql
+++ shop_db.sql
@@ -1,21 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- version 4.2.11
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2022 at 03:43 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: May 01, 2022 at 11:29 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `shop_db`
@@ -27,15 +26,24 @@ SET time_zone = "+00:00";
 -- Table structure for table `cart`
 --
 
-CREATE TABLE `cart` (
-  `id` int(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `cart` (
+`id` int(100) NOT NULL,
   `user_id` int(100) NOT NULL,
   `pid` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `price` int(100) NOT NULL,
   `quantity` int(100) NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `pid`, `name`, `price`, `quantity`, `image`) VALUES
+(51, 33, 26, 'ree', 4, 1, 'cat-1.png'),
+(52, 33, 25, 'samae', 3, 1, 'cat-3.png'),
+(53, 34, 26, 'ree', 4, 1, 'cat-1.png');
 
 -- --------------------------------------------------------
 
@@ -43,8 +51,8 @@ CREATE TABLE `cart` (
 -- Table structure for table `message`
 --
 
-CREATE TABLE `message` (
-  `id` int(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `message` (
+`id` int(100) NOT NULL,
   `user_id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -58,8 +66,8 @@ CREATE TABLE `message` (
 -- Table structure for table `orders`
 --
 
-CREATE TABLE `orders` (
-  `id` int(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `orders` (
+`id` int(100) NOT NULL,
   `user_id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `number` varchar(12) NOT NULL,
@@ -78,14 +86,22 @@ CREATE TABLE `orders` (
 -- Table structure for table `products`
 --
 
-CREATE TABLE `products` (
-  `id` int(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `products` (
+`id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `category` varchar(20) NOT NULL,
   `details` varchar(500) NOT NULL,
   `price` int(100) NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `category`, `details`, `price`, `image`) VALUES
+(25, 'samae', 'vegitables', 'this is a windroufl', 3, 'cat-3.png'),
+(26, 'ree', 'fruits', 'this is abuety ', 4, 'cat-1.png');
 
 -- --------------------------------------------------------
 
@@ -93,14 +109,23 @@ CREATE TABLE `products` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `user_type` varchar(20) NOT NULL DEFAULT 'user',
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`, `image`) VALUES
+(31, 'kasem roomea ', 'kasemro1999@gamil.com', '202cb962ac59075b964b07152d234b70', 'admin', 'pic-3.png'),
+(33, 'mohamad roomea', 'mohamad@roomea.com', '202cb962ac59075b964b07152d234b70', 'admin', 'pic-5.png'),
+(34, 'yhia roomea ', 'yhia@roomea.com', '202cb962ac59075b964b07152d234b70', 'user', 'pic-3.png');
 
 -- --------------------------------------------------------
 
@@ -108,8 +133,8 @@ CREATE TABLE `users` (
 -- Table structure for table `wishlist`
 --
 
-CREATE TABLE `wishlist` (
-  `id` int(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `wishlist` (
+`id` int(100) NOT NULL,
   `user_id` int(100) NOT NULL,
   `pid` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -125,37 +150,37 @@ CREATE TABLE `wishlist` (
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `message`
 --
 ALTER TABLE `message`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `wishlist`
 --
 ALTER TABLE `wishlist`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -165,39 +190,32 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
-
+MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=54;
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
+MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
+MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
-
+MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-
+MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
 --
 -- AUTO_INCREMENT for table `wishlist`
 --
 ALTER TABLE `wishlist`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
-COMMIT;
-
+MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
